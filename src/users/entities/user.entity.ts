5e08b154ed6f4b817fe6399ceace580d9200778a
Kim LeeJun <hwanam1111@naver.com>
2022-02-10 20:52:25 +0900

    Add: create user.

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,36 @@
 import { Column, Entity } from 'typeorm';
 import { CoreEntity } from 'src/common/entities/common.entity';
+import {
+  ObjectType,
+  InputType,
+  Field,
+  registerEnumType,
+} from '@nestjs/graphql';
 
-type UserRole = 'client' | 'owner' | 'delivery';
+enum UserRole {
+  Client,
+  Owner,
+  Delivery,
+}
 
+registerEnumType(UserRole, { name: 'UserRole' });
+
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
   @Column()
+  @Field(() => String)
   email: string;
 
   @Column()
+  @Field(() => String)
   password: string;
 
-  @Column()
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+  })
+  @Field(() => UserRole)
   role: UserRole;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { CreateAccountInput } from './dtos/create-account.dto';
 
 @Injectable()
 export class UsersService {
@@ -9,4 +10,29 @@ export class UsersService {
     @InjectRepository(User)
     private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
+    try {
+      const existsUser = await this.users.findOne({ email });
+      if (existsUser) {
+        return {
+          ok: false,
+          error: 'There is a user with that email already',
+        };
+      }
+
+      await this.users.save(this.users.create({ email, password, role }));
+
+      return { ok: true };
+    } catch (err) {
+      return {
+        ok: false,
+        error: 'Could not create account',
+      };
+    }
+  }
 }
